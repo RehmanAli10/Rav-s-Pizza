7264d76108bad27c3dbfad3bc0d9e04ded2483a7
Rehman Ali <vrehmanali@gmail.com>
2024-02-18 20:42:53 +0500

    search added order

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate(`order/${query}`);
+    setQuery("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;

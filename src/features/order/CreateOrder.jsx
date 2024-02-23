78e7b26ff693fd2edb89108fc36755a194a99b81
Rehman Ali <vrehmanali@gmail.com>
2024-02-23 20:01:24 +0500

    Designed fields

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -61,7 +61,7 @@ function CreateOrder() {
         <label>Address</label>
         <div>
           <input
-            className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400"
+            className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
             type="text"
             name="address"
             required
@@ -71,6 +71,7 @@ function CreateOrder() {
 
       <div>
         <input
+          className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           type="checkbox"
           name="priority"
           id="priority"

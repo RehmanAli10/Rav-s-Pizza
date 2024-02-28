4e3e9efb0d6b9b1e7a530ea6cc1fb78e3d3265e0
Rehman Ali <vrehmanali@gmail.com>
2024-02-28 22:46:59 +0500

    changed error message

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,8 +1,8 @@
 export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
   );
-  if (!res.ok) throw Error("Failed getting address");
+  if (!res.ok) throw Error('There was a problem getting your address');
 
   const data = await res.json();
   return data;
